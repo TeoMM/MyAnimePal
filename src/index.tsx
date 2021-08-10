5e8d77ec566b8396e187666dfe072814789f4bfc
Teo Marginean <teodor.marginean@ibm.com>
2021-08-10 13:10:38 +0300

    wip

--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,41 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { Global, css } from "@emotion/react";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+import App from "./App";
+import React from "react";
+import ReactDOM from "react-dom";
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const indexCSS = css`
+  body {
+    margin: 0;
+    padding: 0;
+    font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", "Roboto", "Oxygen", "Ubuntu",
+      "Cantarell", "Fira Sans", "Droid Sans", "Helvetica Neue", sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    min-height: 100vh;
+    max-width: 100vw;
+  }
+  #root {
+    height: 100%;
+    width: 100%;
+  }
+
+  code {
+    font-family: source-code-pro, Menlo, Monaco, Consolas, "Courier New", monospace;
+  }
+`;
+
+const Index = () => {
+  return (
+    <React.StrictMode>
+      <Global
+        styles={css`
+          ${indexCSS}
+        `}
+      />
+      <App />
+    </React.StrictMode>
+  );
+};
+
+ReactDOM.render(<Index />, document.getElementById("root"));
