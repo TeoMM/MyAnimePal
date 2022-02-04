a5537c583ad4bbde209de0646dc4996dc2ca287d
TeoMM <teodormoffice@gmail.com>
2022-02-04 19:54:37 +0200

    refactor to new stack

--- src/components/Toplist.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-function Toplist() {
-  return <h2>Toplist</h2>;
-}
-
-export default Toplist;
--- src/index.tsx
+++ src/index.tsx
@@ -1,41 +1,67 @@
-import { Global, css } from "@emotion/react";
+/**
+ * index.tsx
+ *
+ * This is the entry file for the application, only setup and boilerplate
+ * code.
+ */
 
-import App from "./App";
-import React from "react";
-import ReactDOM from "react-dom";
+import 'react-app-polyfill/ie11';
+import 'react-app-polyfill/stable';
 
-const indexCSS = css`
-  body {
-    margin: 0;
-    padding: 0;
-    font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", "Roboto", "Oxygen", "Ubuntu",
-      "Cantarell", "Fira Sans", "Droid Sans", "Helvetica Neue", sans-serif;
-    -webkit-font-smoothing: antialiased;
-    -moz-osx-font-smoothing: grayscale;
-    min-height: 100vh;
-    max-width: 100vw;
-  }
-  #root {
-    height: 100%;
-    width: 100%;
-  }
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import FontFaceObserver from 'fontfaceobserver';
 
-  code {
-    font-family: source-code-pro, Menlo, Monaco, Consolas, "Courier New", monospace;
-  }
-`;
+// Use consistent styling
+import 'sanitize.css/sanitize.css';
 
-const Index = () => {
-  return (
-    <React.StrictMode>
-      <Global
-        styles={css`
-          ${indexCSS}
-        `}
-      />
-      <App />
-    </React.StrictMode>
-  );
-};
+import { App } from 'app';
 
-ReactDOM.render(<Index />, document.getElementById("root"));
+import { HelmetProvider } from 'react-helmet-async';
+
+import { configureAppStore } from 'store/configureStore';
+
+import { ThemeProvider } from 'styles/theme/ThemeProvider';
+
+import reportWebVitals from 'reportWebVitals';
+
+// Initialize languages
+import './locales/i18n';
+
+// Observe loading of Inter (to remove 'Inter', remove the <link> tag in
+// the index.html file and this observer)
+const openSansObserver = new FontFaceObserver('Inter', {});
+
+// When Inter is loaded, add a font-family using Inter to the body
+openSansObserver.load().then(() => {
+  document.body.classList.add('fontLoaded');
+});
+
+const store = configureAppStore();
+const MOUNT_NODE = document.getElementById('root') as HTMLElement;
+
+ReactDOM.render(
+  <Provider store={store}>
+    <ThemeProvider>
+      <HelmetProvider>
+        <React.StrictMode>
+          <App />
+        </React.StrictMode>
+      </HelmetProvider>
+    </ThemeProvider>
+  </Provider>,
+  MOUNT_NODE,
+);
+
+// Hot reloadable translation json files
+if (module.hot) {
+  module.hot.accept(['./locales/i18n'], () => {
+    // No need to render the App again because i18next works with the hooks
+  });
+}
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
